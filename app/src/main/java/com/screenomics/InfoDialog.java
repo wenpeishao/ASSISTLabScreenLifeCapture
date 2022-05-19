0f01bd5c327b5334b233e0b421bea8ebd934b34c
Matthew Curran <mjcurran@ucdavis.edu>
2022-05-19 15:30:59 -0400

    feat: Visual tweaks, ND branding

--- app/src/main/java/com/screenomics/InfoDialog.java
+++ app/src/main/java/com/screenomics/InfoDialog.java
@@ -12,9 +12,9 @@ public class InfoDialog extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle("SUTD ScreenLife Capture")
-                .setMessage("Developed by researchers from the Singapore University of Technology and Design, SUTD ScreenLife Capture is a smartphone application that captures high frequency screenshots of your smartphone use. When in use, the data is securely encrypted and sent over to the research team at SUTD.\n\n" +
-                        "This application is meant for research purposes only, and if you are not a consenting participant of a research study conducted by Singapore University of Technology and Design researchers, please delete this application immediately.")
+        builder.setTitle("ND ScreenLife Capture")
+                .setMessage("Developed by researchers from the Singapore University of Technology and Design, ScreenLife Capture is a smartphone application that captures high frequency screenshots of your smartphone use. When in use, the data is securely encrypted and sent over to the research team at Notre Dame.\n\n" +
+                        "This application is meant for research purposes only, and if you are not a consenting participant of a research study conducted by University of Notre Dame researchers, please delete this application immediately.")
                 .setNeutralButton("OK", null);
         return builder.create();
     }
