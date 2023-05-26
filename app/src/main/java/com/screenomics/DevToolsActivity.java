8396fd717b0aa6172d36aac45b208a20e8af7318
Matthew Curran <mjcurran@ucdavis.edu>
2023-05-26 09:19:55 -0400

    fix:  Updates to make it work on latest android os

--- app/src/main/java/com/screenomics/DevToolsActivity.java
+++ app/src/main/java/com/screenomics/DevToolsActivity.java
@@ -2,19 +2,31 @@ package com.screenomics;
 
 import android.app.Activity;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 public class DevToolsActivity extends Activity {
 
     String imagesPath;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +42,37 @@ public class DevToolsActivity extends Activity {
         Button setMaxSendButton = findViewById(R.id.setMaxSendButton);
         EditText maxSendInput = findViewById(R.id.maxSendInput);
         Button disableButton = findViewById(R.id.disableButton);
+        Button wifiButton = findViewById(R.id.testWifiButton);
+        Button mobileButton = findViewById(R.id.testMobileButton);
+        TextView wifiResult = findViewById(R.id.wifiResultText);
+        TextView mobileResult = findViewById(R.id.mobileResultText);
 
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         batchSizeInput.setText(String.valueOf(prefs.getInt("batchSize", Constants.BATCH_SIZE_DEFAULT)));
         maxSendInput.setText(String.valueOf(prefs.getInt("maxSend", Constants.MAX_TO_SEND_DEFAULT)));
+
+        wifiButton.setOnClickListener(view -> {
+            if(InternetConnection.checkWiFiConnection(getApplicationContext())){
+                wifiResult.setText("OK");
+            }else{
+                wifiResult.setText("No WiFi");
+            }
+
+        });
+
+        mobileButton.setOnClickListener(v -> {
+            if(InternetConnection.checkMobileDataConnection(getApplicationContext())){
+                mobileResult.setText("OK");
+            }else{
+                mobileResult.setText("No Cell");
+            }
+        });
+
+        /*
+        endpointTestButton.setOnClickListener(v -> {
+            new NetTester().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, null);
+
+        });*/
 
         clearImagesButton.setOnClickListener(v -> {
             File dir = new File(imagesPath);
@@ -105,5 +144,31 @@ public class DevToolsActivity extends Activity {
             editor.apply();
             finish();
         });
+    }
+
+
+    /*public class NetTester extends AsyncTask<Void, Integer, Void>{
+        @Override
+        protected Void doInBackground(Void... params){
+            Response response = canReachEndpoint();
+            mobileEndpointResult.setText(response.body().toString());
+            return null;
+        }
+    }*/
+
+    public Response canReachEndpoint(){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(Constants.UPLOAD_ADDRESS).get().build();
+
+        Response response = null;
+        try {
+            //long startTime = System.nanoTime();
+            response = client.newCall(request).execute();
+            Log.d("SCREENOMICS_TEST", response.toString());
+        } catch (IOException e) {
+            Log.d("SCREENOMICS_ERROR", e.toString());
+            e.printStackTrace();
+        }
+        return response;
     }
 }
