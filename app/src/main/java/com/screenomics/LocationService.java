39b18945049615906b8cf7975b1d06928875b0ab
Matthew Curran <mjcurran@ucdavis.edu>
2024-01-02 14:29:45 -0500

    feat:  Basic LocationService functional.

--- app/src/main/java/com/screenomics/LocationService.java
+++ app/src/main/java/com/screenomics/LocationService.java
@@ -17,7 +17,7 @@ import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
-import androidx.annotation.RequiresApi;
+
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.ServiceCompat;
 
@@ -27,11 +27,11 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.Priority;
-import com.google.android.gms.tasks.OnSuccessListener;
+
 
 public class LocationService extends Service {
 
-    private static final String CHANNEL_ID = "screenomics_location_id";
+    private static final String LOCATION_CHANNEL_ID = "screenomics_location_id";
 
     private FusedLocationProviderClient fusedLocationClient;
 
@@ -39,9 +39,11 @@ public class LocationService extends Service {
 
     @Override
     public void onCreate(){
+        Log.d("SCREENOMICS", "Location Service onCreate");
         super.onCreate();
         createLocationRequest();
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
     }
 
     //...
@@ -61,15 +63,16 @@ public class LocationService extends Service {
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-
+        Log.d("SCREENOMICS", "Location Service Bound");
 
         return new LocalBinder();
     }
 
     private void createNotificationChannel() {
+        Log.d("SCREENOMICS", "Creating Notification Channel");
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel serviceChannel = new NotificationChannel(
-                    CHANNEL_ID,
+                    LOCATION_CHANNEL_ID,
                     "Screenomics Location Channel",
                     NotificationManager.IMPORTANCE_HIGH
             );
@@ -89,7 +92,9 @@ public class LocationService extends Service {
     };
 
     private void startLocationUpdates() {
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        Log.d("SCREENOMICS", "Starting Location Updates");
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
@@ -97,6 +102,7 @@ public class LocationService extends Service {
             //                                          int[] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
+            Log.d("SCREENOMICS", "No Location Permissions");
             return;
         }
         fusedLocationClient.requestLocationUpdates(locationRequest,
@@ -106,7 +112,7 @@ public class LocationService extends Service {
 
     @Override
     public int onStartCommand(Intent receivedIntent, int flags, int startId) {
-
+        Log.d("SCREENOMICS", "Location Service Started");
         createNotificationChannel();
 
         Intent notificationIntent = new Intent(this, MainActivity.class);
@@ -119,14 +125,16 @@ public class LocationService extends Service {
 
         PendingIntent pendingIntent = PendingIntent.getActivity(this,0, notificationIntent, intentflags);
 
-        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setContentTitle(getString(R.string.app_name))
+        Notification notification = new NotificationCompat.Builder(this, LOCATION_CHANNEL_ID)
+                .setContentTitle("SCREENOMICS")
                 .setContentTitle("Screenomics Location Updates Running")
-                .setSmallIcon(R.mipmap.ic_launcher)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
                 .setContentIntent(pendingIntent)
                 .build();
         //startForeground(AppConstants.LOCATION_SERVICE_NOTIF_ID, notification);
-        ServiceCompat.startForeground(this,1, notification, ServiceInfo.FOREGROUND_SERVICE_TYPE_LOCATION);
+        ServiceCompat.startForeground(this,22, notification, ServiceInfo.FOREGROUND_SERVICE_TYPE_LOCATION);
+
+        startLocationUpdates();
 
         return START_STICKY;
     }
